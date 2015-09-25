06b5cff5787d87da8547a15dca6972368df1e637
Rory Anderson <baloogan@gmail.com>
2015-09-25 02:41:47 -0700

    Added nuget packages, starting on Startup.cs and Web.config.

--- Escalation/Startup.cs
+++ Escalation/Startup.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 using Owin;
+using System;
+using System.Reflection;
 
 [assembly: OwinStartupAttribute(typeof(Escalation.Startup))]
 namespace Escalation
 {
     public partial class Startup
     {
+        public static DateTime compilationDateTime = getCreationDate();
+        public static string version = "Escalation Alpha 1";
+        private static DateTime getCreationDate()
+        {
+            return System.IO.File.GetLastWriteTime(Assembly.GetExecutingAssembly().Location);
+        }
         public void Configuration(IAppBuilder app)
         {
+
+            var hubConfiguration = new HubConfiguration();
+            hubConfiguration.EnableDetailedErrors = true;
             ConfigureAuth(app);
+            app.MapSignalR(hubConfiguration);
         }
     }
 }
